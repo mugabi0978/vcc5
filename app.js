7105451a74015e7cce1c6134b66ef1009e3e3837
Powell09 <powellorenz@gmail.com>
2019-06-10 16:14:44 +0300

    standard app 1

---
+++ app.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// routes
+import Home from './components/home'
+import Agent from './components/agent'
+import Caller from './components/caller'
+import EndCall from './components/end-call'
+import EndCallDrSession from './components/end-call-doctor'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: Home },
+    { path: '/agent', component: Agent },
+    { path: '/caller', component: Caller },
+    { path: '/end', component: EndCall },
+    { path: '/enddrsession', component: EndCallDrSession }
+  ]
+})
+
+new Vue({
+  router
+  // render: h => h(App)
+}).$mount('#app')
---
+++ components/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="uk-section uk-section-primary">
+      <div class="uk-container uk-text-center">
+        <h1 class="uk-margin-large-top">CTI Africa Call the Clinic</h1>
+        <p class="uk-text-lead">A video call center where callers queue up to talk to available doctors.</p>
+      </div>
+    </div>
+    <div class="uk-section uk-section-default">
+      <div class="container">
+        <div class="route-home uk-flex-center" uk-grid>
+          <div>
+            <router-link to="/agent" class="uk-button uk-button-primary uk-button-large"> Join as Doctor </router-link>
+          </div>
+          <!-- <div>
+            <router-link to="/caller" class="uk-button uk-button-primary uk-button-large">Join as caller</router-link>
+          </div> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
